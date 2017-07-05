a05a3af666255cee16c0f4d81a6e5a7f29c7bb1e
CalJersey <crippe@gmail.com>
2017-07-04 18:44:41 -0700

    switched away from bootstrap to more customized css for bord layout

--- js/app.js
+++ js/app.js
@@ -2,5 +2,13 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  console.log('jQuery ready function fired.');
+  $("#board").on("click", function(event){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();
+  });
 
 });
